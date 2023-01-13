22242081a018dd0473626ed3b7ff40031d80782b
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2023-01-13 04:32:31 -0300

    Merge config pkg into app pkg

--- app/setup.go
+++ app/setup.go
@@ -1,17 +1,20 @@
 package app
 
 import (
-	"github.com/bmdavis419/go-backend-template/config"
+	"os"
+
 	"github.com/bmdavis419/go-backend-template/database"
 	"github.com/bmdavis419/go-backend-template/router"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/swagger"
+	"github.com/joho/godotenv"
 )
 
 func Setup() (*fiber.App, error) {
 	// load env
-	err := config.LoadENV()
+	err := loadENV()
 	if err != nil {
 		return nil, err
 	}
@@ -35,7 +38,23 @@ func Setup() (*fiber.App, error) {
 	router.SetupRoutes(app)
 
 	// attach swagger
-	config.AddSwaggerRoutes(app)
+	addSwaggerRoute(app)
 
 	return app, nil
+}
+
+func loadENV() error {
+	goEnv := os.Getenv("GO_ENV")
+	if goEnv == "" || goEnv == "development" {
+		err := godotenv.Load()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func addSwaggerRoute(app *fiber.App) {
+	// setup swagger
+	app.Get("/swagger/*", swagger.HandlerDefault)
 }
